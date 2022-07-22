78e070047dc549333063e795f4ef8b49f49b26f1
mathocihara <matheusyukio.hocihara@gmail.com>
2022-07-22 00:06:52 -0300

    estilização dos componentes

--- my-app/src/componetes/Task.js
+++ my-app/src/componetes/Task.js
@@ -1,7 +1,12 @@
 import React from "react";
+import '../componetes/Task.css'
  
 const Task = ({task}) => {
-    return (  <h1>{task.title}</h1> );
+    return (  <div className="task-container">
+
+    <h1>{task.title}</h1>
+
+    </div> );
 }
  
 export default Task;
